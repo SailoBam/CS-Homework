666731c630f6c28a618f5388de630b1392b25fd7
SailoBam <francisbamsey@gmail.com>
2023-11-14 21:21:56 +0000

    initial file commit

---
+++ HW141123/Homework Integer Convert/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+String input;
+int i;
+
+while (true) {
+    Console.WriteLine("Entry an integer");
+    input = Console.ReadLine();
+    if(input=="exit") break;
+        try {
+        i = Convert.ToInt16(input);
+        Console.WriteLine("You entered "+ i);
+        } catch (Exception myExp) {
+            Console.WriteLine("Could Not convert the input to an integer");
+        } 
+}
+
+Console.WriteLine("Done");
